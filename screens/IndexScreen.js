70e1a98806f7b046b5a058afac61fa5ba9269d4a
Cihan Yuksel <cihan_yuksel26@hotmail.com>
2024-10-31 16:45:46 +0300

    navigation and indexscreen

---
+++ screens/IndexScreen.js
@@ -0,0 +1,12 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+export default function IndexScreen() {
+  return (
+    <View>
+      <Text>IndexScreen</Text>
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({})
