25fc2bc8e441cdccbd6413809c859c45824425d2
Cihan Yuksel <cihan_yuksel26@hotmail.com>
2024-10-31 16:51:00 +0300

    createscreen

--- App.js
+++ App.js
@@ -2,14 +2,17 @@ import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View } from "react-native";
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import IndexScreen from "./screens/IndexScreen";
+import CreateScreen from "./screens/CreateScreen";
 
 const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
     <NavigationContainer>
-      <Stack.Navigator>
-        <Stack.Screen name="Home" component={HomeScreen} />
+      <Stack.Navigator screenListeners={{headerTitle:'Blog Uygulaması'}}>
+        <Stack.Screen name="Index" component={IndexScreen} />
+        <Stack.Screen name="Create" component={CreateScreen} />
       </Stack.Navigator>
     </NavigationContainer>
   );
