f5cb58ebce9c17dc9bcf0afdb90bcca04dd43109
Cihan Yuksel <cihan_yuksel26@hotmail.com>
2024-11-01 20:32:22 +0300

    EditScreen

--- App.js
+++ App.js
@@ -1,11 +1,14 @@
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, Text, TouchableOpacity, View } from "react-native";
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import IndexScreen from "./screens/IndexScreen";
 import CreateScreen from "./screens/CreateScreen";
-import {  Provider } from "./context/BlogContext";
+import { Provider } from "./context/BlogContext";
 import ShowScreen from "./screens/ShowScreen";
+import AntDesign from "@expo/vector-icons/AntDesign";
+import EditScreen from "./screens/EditScreen";
+import EvilIcons from "@expo/vector-icons/EvilIcons";
 
 const Stack = createNativeStackNavigator();
 
@@ -13,10 +16,31 @@ export default function App() {
   return (
     <Provider>
       <NavigationContainer>
-        <Stack.Navigator screenOptions={{ headerTitle: 'Blog Uygulaması '}}>
-          <Stack.Screen name="Index" component={IndexScreen} />
+        <Stack.Navigator screenOptions={{ headerTitle: "Blog Uygulaması " }}>
+          <Stack.Screen
+            name="Index"
+            component={IndexScreen}
+            options={({ navigation }) => ({
+              headerRight: () => (
+                <TouchableOpacity onPress={() => navigation.navigate("Create")}>
+                  <AntDesign name="plus" size={24} color="black" />
+                </TouchableOpacity>
+              ),
+            })}
+          />
           <Stack.Screen name="Create" component={CreateScreen} />
-          <Stack.Screen name="Show" component={ShowScreen} />
+          <Stack.Screen
+            name="Show"
+            component={ShowScreen}
+            options={({ navigation }) => ({
+              headerRight: () => (
+                <TouchableOpacity onPress={() => navigation.navigate("Edit")}>
+                  <EvilIcons name="pencil" size={35} color="black" />
+                </TouchableOpacity>
+              ),
+            })}
+          />
+          <Stack.Screen name="Edit" component={EditScreen} />
         </Stack.Navigator>
       </NavigationContainer>
     </Provider>
