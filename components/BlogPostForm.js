d0d4abe5d627cc93daafd018bb8ed4049f772e50
Cihan Yuksel <cihan_yuksel26@hotmail.com>
2024-11-01 21:38:19 +0300

    BlogPostForm

--- components/BlogPostForm.js
+++ components/BlogPostForm.js
@@ -1,12 +1,27 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import {
+  StyleSheet,
+  Text,
+  TextInput,
+  TouchableOpacity,
+  View,
+} from "react-native";
+import React from "react";
 
 export default function BlogPostForm() {
+  const [title, setTitle] = useState("");
+  const [content, setContent] = useState("");
+
   return (
     <View>
-      <Text>BlogPostForm</Text>
+      <Text>Başlığı Giriniz:</Text>
+      <TextInput value={title} onChangeText={(text) => setTitle(text)} />
+      <Text>İçeriği Giriniz:</Text>
+      <TextInput value={content} onChangeText={(text) => setContent(text)} />
+      <TouchableOpacity>
+        <Text>Kaydet</Text>
+      </TouchableOpacity>
     </View>
-  )
+  );
 }
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({});
